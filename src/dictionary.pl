46a68d230867948edfad8ec401132b881632c2fa
Matt Fenwick <mfenwick@student.uchc.edu>
2011-11-11 18:54:35 -0600

    get dictionary up and running -- GUI starts, but plumbing for definitions not yet connected

--- src/dictionary.pl
+++ src/dictionary.pl
@@ -4,24 +4,16 @@ use strict;
 use warnings;
 use Tkx;
 use GUI;
-
-print "hi";
-my $gui = GUI->new(3);
-print "hi";
-&Tkx::MainLoop();
-print "hi";
+use Controller;
 
 
-# &simple_gui();
+&simple_gui();
 
 
 sub simple_gui {
-    my $mw = &make_mainwindow();
-    my $ent_but_frame = &make_entry_button_frame($mw);
-    my $text_frame = &make_texts($mw);
-    
-    $ent_but_frame->g_grid(-row => 0, -column => 0);
-    $text_frame->g_grid(-row => 0, -column => 1, -rowspan => 20);
+    my $controller = Controller->new();
+    my $gui = GUI->new($controller);
+    $controller->setGUI($gui);
     
     Tkx::MainLoop;
 }
